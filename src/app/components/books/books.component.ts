604931cb7f6a71383cbcaea006d166a2fab2cf11
iqbal9865 <iqbalahmed9865@gmail.com>
2022-08-19 16:36:43 +0600

    show books

---
+++ src/app/components/books/books.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.scss']
+})
+export class BooksComponent implements OnInit {
+
+  constructor() { }
+
+  books: any = [
+    {
+      name: 'sellowship of the ring',
+      img: 'https://m.media-amazon.com/images/I/51SZ+xIbZnL._SL320_.jpg',
+      price: 270
+    },
+    {
+      name: 'the unspoken name',
+      img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1576631770l/45046552.jpg',
+      price: 380
+    },
+    {
+      name: 'the book thief',
+      img: 'http://ecx.images-amazon.com/images/I/51eQvANUsnL.jpg',
+      price: 460
+    },
+    {
+      name: 'sellowship of the ring',
+      img: 'https://m.media-amazon.com/images/I/51SZ+xIbZnL._SL320_.jpg',
+      price: 270
+    },
+    {
+      name: 'the unspoken name',
+      img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1576631770l/45046552.jpg',
+      price: 380
+    },
+    {
+      name: 'the book thief',
+      img: 'http://ecx.images-amazon.com/images/I/51eQvANUsnL.jpg',
+      price: 270
+    }
+  ]
+
+
+  ngOnInit(): void {
+    console.log(this.books);
+
+  }
+
+
+
+}
